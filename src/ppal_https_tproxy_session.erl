9317e3bf2c4015edc72a524757cf96522f58baf8
Geoff Reedy <geoff@programmer-monk.net>
2018-06-19 11:23:47 -0600

    Wait for own_socket before further processing in https tproxy

--- src/ppal_https_tproxy_session.erl
+++ src/ppal_https_tproxy_session.erl
@@ -6,7 +6,8 @@
 -export([start_link/1]).
 -export([init/1, callback_mode/0, terminate/3]).
 
--export([wait_for_proxy_response/3, read_resp_headers/3, forward_data/3]).
+-export([wait_for_proxy_response/3, read_resp_headers/3, forward_data/3,
+         wait_for_socket_own/3]).
 
 -record(data, {localsocket, remotesocket,
                remotedata = <<>>}).
@@ -18,27 +19,32 @@ callback_mode() ->
     [state_functions, state_enter].
 
 init(ClientSocket) ->
-    {ok, {Ip, Port}} = inet:sockname(ClientSocket),
+    {ok, wait_for_socket_own, #data{localsocket=ClientSocket, remotesocket=undefined}}.
+
+wait_for_socket_own(enter, _, _) ->
+    keep_state_and_data;
+wait_for_socket_own(info, {tcp_closed, _}, _Data) ->
+    stop;
+wait_for_socket_own(info, own_socket, Data=#data{localsocket=ClientSocket}) ->
+    {ok, {Ip, _Port}} = inet:sockname(ClientSocket),
     IpString = inet:ntoa(Ip),
     UrlStr = lists:flatten(["https://", IpString]),
     [FirstProxy|_Proxies] = ppal_proxylookup:proxies_for_url(UrlStr),
-    {ok, ParsedProxy={ProxyScheme, _UserInfo, _Host, _Port}} = ppal_uri:parse_uri(FirstProxy),
+    {ok, ParsedProxy={ProxyScheme, _UserInfo, _Host, _PPort}} = ppal_uri:parse_uri(FirstProxy),
     case ProxyScheme of
-        "direct" -> direct_connect(ClientSocket, Ip, 443);
-        "http" -> http_connect(ClientSocket, Ip, 443, ParsedProxy)
+        "direct" -> direct_connect(Data, Ip, 443);
+        "http" -> http_connect(Data, Ip, 443, ParsedProxy)
     end.
 
-direct_connect(ClientSocket, Ip, Port) ->
+direct_connect(Data, Ip, Port) ->
     {ok, Socket} = gen_tcp:connect(Ip, Port,
-                                   [binary, {active, false},
-                                    {raw, ?SOL_SOCKET, ?SO_MARK, ?IntOpt(101)}]),
-    {ok, forward_data, #data{localsocket=ClientSocket, remotesocket=Socket}}.
+                                   [binary, {active, false}]),
+    {next_state, forward_data, Data#data{remotesocket=Socket}}.
 
-http_connect(ClientSocket, Ip, Port, {"http", ProxyUserInfo, ProxyHost, ProxyPort}) ->
-    ActualPort = case ProxyPort of undefined -> 80; _ -> list_to_integer(ProxyPort) end,
-    {ok, Socket} = gen_tcp:connect(ProxyHost, ActualPort,
-                                   [binary, {active, false},
-                                    {raw, ?SOL_SOCKET, ?SO_MARK, ?IntOpt(101)}]),
+http_connect(Data, Ip, Port, {"http", ProxyUserInfo, ProxyHost, ProxyPort}) ->
+    ProxyPortNum = case ProxyPort of undefined -> 80; _ -> list_to_integer(ProxyPort) end,
+    {ok, Socket} = gen_tcp:connect(ProxyHost, ProxyPortNum,
+                                   [binary, {active, false}]),
     ok = gen_tcp:send(Socket, ["CONNECT ", inet:ntoa(Ip), ":", integer_to_list(Port), " HTTP/1.1\r\n"]),
     case ProxyUserInfo of
         undefined -> ok;
@@ -47,21 +53,21 @@ http_connect(ClientSocket, Ip, Port, {"http", ProxyUserInfo, ProxyHost, ProxyPor
                                         "\r\n"])
     end,
     ok = gen_tcp:send(Socket, "\r\n"),
-    {ok, wait_for_proxy_response, #data{localsocket=ClientSocket, remotesocket=Socket}}.
+    {next_state, wait_for_proxy_response, Data#data{remotesocket=Socket}}.
 
-wait_for_proxy_response(enter, _OldState, _Data) ->
-    keep_state_and_data;
-wait_for_proxy_response(info, own_socket, Data) ->
+wait_for_proxy_response(enter, _OldState, Data) ->
     inet:setopts(Data#data.remotesocket, [{active, once}, binary, {packet, raw}]),
     keep_state_and_data;
-wait_for_proxy_response(info, {tcp, RemoteSocket, Pkt}, Data=#data{localsocket=LocalSocket, remotesocket=RemoteSocket}) ->
+wait_for_proxy_response(info, {tcp_closed, _}, _Data) ->
+    stop;
+wait_for_proxy_response(info, {tcp, RemoteSocket, Pkt}, Data=#data{remotesocket=RemoteSocket}) ->
     NewPkt = <<(Data#data.remotedata)/binary, Pkt/binary>>,
     case erlang:decode_packet(http, NewPkt, []) of
-        {ok, {http_response, HttpVersion, 200, Message}, Rest} ->
+        {ok, {http_response, _HttpVersion, 200, _Message}, Rest} ->
             {next_state, read_resp_headers, Data#data{remotedata=(<<>>)},
              {next_event, info, {tcp, RemoteSocket, Rest}}};
-        {ok, {http_response, HttpVersion, Status, Message}, Rest} ->
-            {stop, {Status, Message}};
+        {ok, {http_response, _HttpVersion, Status, Message}, _Rest} ->
+            {stop, {proxy_connect_error, Status, Message}};
         {more, _Length} ->
             inet:setopts(RemoteSocket, [{active, once}, {packet, raw}]),
             {keep_state, Data#data{remotedata=NewPkt}};
@@ -71,7 +77,9 @@ wait_for_proxy_response(info, {tcp, RemoteSocket, Pkt}, Data=#data{localsocket=L
 
 read_resp_headers(enter, _OldState, _Data) ->
     keep_state_and_data;
-read_resp_headers(info, {tcp, RemoteSocket, Pkt}, Data=#data{localsocket=LocalSocket, remotesocket=RemoteSocket}) ->
+read_resp_headers(info, {tcp_closed, _}, _Data) ->
+    stop;
+read_resp_headers(info, {tcp, RemoteSocket, Pkt}, Data=#data{remotesocket=RemoteSocket}) ->
     NewPkt = <<(Data#data.remotedata)/binary, Pkt/binary>>,
     case erlang:decode_packet(httph, NewPkt, []) of
         {ok, http_eoh, Rest} ->
